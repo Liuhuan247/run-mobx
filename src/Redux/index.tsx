105c21712b1467368ec18e053efbd98231f35d5a
henry.liu <henry.liu@ximalaya.com>
2020-06-13 13:50:25 +0800

    redux

---
+++ src/Redux/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+
+const Counter: FC = () => {
+    return (
+        <>
+            <p></p>
+            <button>增加</button>
+        </>
+    );
+};
+
+export default Counter;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { FC } from "react";
 import ReactDOM from "react-dom";
 import Timer from "./observer";
-import { observable } from "mobx";
+import { observable, autorun } from "mobx";
+import { Provider, observer, inject, useObserver } from "mobx-react";
+import Store from "./store";
+// const arrayData = observable([
+//     {
+//         id: 0,
+//         name: "zhangsan",
+//         age: 13,
+//     },
+// ]);
+
+// autorun(() => {
+//     console.log(arrayData.length);
+// });
+
+// arrayData.push({ id: 2, name: "lisi", age: 19 });
 
 const timerData = observable({
     secondsPassed: 0,
@@ -9,11 +24,40 @@ const timerData = observable({
 
 export type timerType = typeof timerData;
 
-setInterval(() => {
-    timerData.secondsPassed++;
-}, 1000);
+// setInterval(() => {
+//     timerData.secondsPassed++;
+// }, 1000);
+
+// useObserver((props) => {
+//     let i = 0;
+//     setTimeout(() => {
+//         props.store.add({
+//             id: i++,
+//             name: `zhang`,
+//             age: 17,
+//         });
+//     }, 1000);
+// })
+const Demo: FC = () => {
+    return <div>测试</div>;
+};
+
+interface storeType {
+    id: number;
+    name: string;
+    age: number;
+}
+
+let store = new Store<storeType>();
+
+// setInterval(() => {
+//     store.add({ id: 1, name: "zhgan", age: 19 });
+// }, 1000);
 
 ReactDOM.render(
-    <Timer timeData={timerData} />,
+    <Provider store={store}>
+        {/* <Timer timeData={timerData} />, */}
+        <Demo />
+    </Provider>,
     document.getElementById("root")
 );
