62abf6e85ff17d0d3e976b4436a84143940831bc
刘焕 <liuhuan247_2014@163.com>
2020-06-27 10:33:48 +0800

    删除无用代码

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MobxCounter from "./mobx";
-import { observable } from "mobx";
 import { Provider } from "mobx-react";
 import Store from "./mobx/store";
 
-const timerData = observable({
-    secondsPassed: 0,
-});
-
-export type timerType = typeof timerData;
-
-setInterval(() => {
-    timerData.secondsPassed++;
-}, 1000);
-
 let store = new Store();
-
-export type StoreType = typeof store;
 
 ReactDOM.render(
     <Provider store={store}>
